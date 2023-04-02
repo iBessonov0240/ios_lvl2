f193df6aaaebcbdc149681d514ad3d4e802f79b6
Ilia <ilya2491@gmail.com>
2023-04-02 18:41:58 +0300

    exercise done

--- m5_lists/M5_Homework/M5_Homework/SwiftUIView.swift
+++ m5_lists/M5_Homework/M5_Homework/SwiftUIView.swift
@@ -1,8 +1,105 @@
 import SwiftUI
 
 struct SwiftUIView: View {
+    @State private var selection = 0
+    let data = DataAnimals()
+    @State var isExpanded = false
+    var columns: [GridItem] =
+    [.init(.fixed(90))]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView(selection: $selection) {
+            ScrollView {
+                LazyVStack(alignment: .leading) {
+                    ForEach(data.animals) { animal in
+                        Text(animal.name)
+                            .font(.headline)
+                            .foregroundColor(Color.blue)
+                        HStack {
+                            VStack(alignment: .leading) {
+                                ForEach(animal.breeds, id: \.name) { bread in
+                                    HStack {
+                                        AsyncImage(url: URL(string: bread.url), placeholder: {
+                                            Text(verbatim: "Loading...")
+                                        })
+                                        .frame(width: 200, height: 200)
+                                        .clipShape(Circle())
+
+                                        Text(bread.name)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            .tabItem {
+                VStack {
+                    Image(systemName: "1.circle")
+                    Text("LazyVStack")
+                }
+            }
+            .tag(0)
+
+            ScrollView {
+                LazyVGrid(columns: columns, alignment: .leading) {
+                    LazyVStack(alignment: .leading) {
+                        ForEach(data.animals) { animal in
+                            Text(verbatim: animal.name)
+                                .font(.headline)
+                                .foregroundColor(Color.blue)
+                            VStack(alignment: .center) {
+                            ForEach(animal.breeds, id: \.name) { bread in
+                                AsyncImage(url: URL(string: bread.url), placeholder: {
+                                    Text(verbatim: "Loading...")
+                                })
+                                .frame(width: 200, height: 200)
+                                .clipShape(Circle())
+                                .padding(.bottom, 100)
+                                .overlay(Text(bread.name)
+                                    .background {
+                                        RoundedRectangle(cornerRadius: 5)
+                                            .foregroundColor(.white)
+                                    })
+                            }
+                        }
+                    }
+                }
+            }
+        }
+                .tabItem {
+                    VStack {
+                        Image(systemName: "2.circle")
+                        Text("LazyGrid")
+                    }
+                }
+                .tag(1)
+
+            List(data.animals) { animal in
+                DisclosureGroup(animal.name) {
+                    VStack(alignment: .leading) {
+                        ForEach(animal.breeds, id: \.name) { breed in
+                            HStack {
+                                AsyncImage(url: URL(string: breed.url), placeholder: {
+                                    Text(verbatim: "Loading...")
+                                    })
+                                    .frame(width: 100, height: 100)
+                                    .clipShape(Circle())
+                                    Text(breed.name)
+                                    }
+                                    .padding(.trailing)
+                                    }
+                                }
+                            }
+                        }
+                .tabItem {
+                    VStack {
+                        Image(systemName: "3.circle")
+                        Text("List")
+                    }
+                }
+                .tag(2)
+        }
     }
 }
 
