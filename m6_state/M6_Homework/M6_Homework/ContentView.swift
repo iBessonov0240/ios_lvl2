3259c5f809ddceffa669f270a83787408572e8a9
Ilia <ilya2491@gmail.com>
2023-04-14 15:28:33 +0300

    exercise done

--- m6_state/M6_Homework/M6_Homework/ContentView.swift
+++ m6_state/M6_Homework/M6_Homework/ContentView.swift
@@ -4,8 +4,51 @@ struct ContentView: View {
     @ObservedObject var viewModel: ViewModel
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(viewModel.animals) { animal in
+                NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                    HStack {
+                        Text(animal.name)
+                    }
+                }
+            }
+            .navigationTitle("Животные")
+            .onAppear {
+                viewModel.fetchAnimals()
+            }
+            .overlay(Group {
+                if viewModel.isLoading {
+                    ProgressView("Загрузка...")
+                } else if let errorMessage = viewModel.errorMessage {
+                    VStack {
+                        Text(errorMessage)
+                        Button("Обновить", action: viewModel.fetchAnimals)
+                    }
+                }
+            })
+        }
+    }
+}
+
+struct AnimalDetailView: View {
+    @EnvironmentObject var viewModel: ViewModel
+    let animal: Animal
+
+    var body: some View {
+    ScrollView {
+        VStack {
+            ForEach(animal.breeds, id: \.name) { bread in
+                AsyncImage(url: URL(string: bread.url), placeholder: {
+                    Text(verbatim: "Loading...")
+                })
+                .scaledToFit()
+                .frame(height: 200)
+                Text(bread.name)
+                    .font(.largeTitle)
+                Spacer()
+                }
+            }
+        }
     }
 }
 
--- m6_state/M6_Homework/M6_Homework/ViewModel.swift
+++ m6_state/M6_Homework/M6_Homework/ViewModel.swift
@@ -2,14 +2,22 @@ import Foundation
 
 final class ViewModel: ObservableObject {
     let service: UnstableNetworkService
+    @Published var animals: [Animal] = []
+    @Published var isLoading = false
+    @Published var errorMessage: String?
     
     init(service: UnstableNetworkService) {
         self.service = service
-        
+    }
+
+    func fetchAnimals() {
+        isLoading = true
+        errorMessage = nil
         service.getData { result in
-            print(result)
+            self.isLoading = false
+            self.animals = result
         } failure: { error in
-            print(error)
+            self.errorMessage = error.localizedDescription
         }
     }
 }
